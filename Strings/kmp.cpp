a0b8fa5ff9aad4d6ae7f44140fc644946f920b6c
Ziad El-Gafy <74333133+ZiadElGafy@users.noreply.github.com>
2023-04-21 09:26:13 +0200

    Update kmp.cpp

--- Strings/kmp.cpp
+++ Strings/kmp.cpp
@@ -12,40 +12,40 @@ KMP: O(n + m)
 
 Prefix function: the length of the longest proper prefix which is also a suffix of a string
 
-lps[N]: an array where lps[i] = the prefix function of substring(0, i)
+pi[N]: an array where pi[i] = the prefix function of substring(0, i)
 
 Idx:	0 1 2 3 4 5 6 7 8 9
 string: 	a b d a b c a b c a
-lps: 	0 0 0 1 2 0 1 2 0 1
+pi: 	0 0 0 1 2 0 1 2 0 1
 
 Idx:	0 1 2 3 4 5 6 7 8 9
 string: 	a b c a b c a b c a
-lps: 	0 0 0 1 2 3 4 5 6 7
+pi: 	0 0 0 1 2 3 4 5 6 7
 
-lps[0] = 0 because there is no proper prefix of a string of 1 character
+pi[0] = 0 because there is no proper prefix of a string of 1 character
 
-lps[i + 1] <= lps[i] + 1
+pi[i + 1] <= pi[i] + 1
 */
 
-vector<int> prefixFunction(string &pattern) {
-    int n = pattern.size();
-    vector<int> lps(n);
-    lps[0] = 0;
+vector<int> prefixFunction(string &s) {
+    int n = s.size();
+    vector<int> pi(n);
+    pi[0] = 0;
     
     for (int i = 1; i < n; i++) {
-        lps[i] = 0;
-        int j = lps[i - 1];
+        pi[i] = 0;
+        int j = pi[i - 1];
 
-        while (j > 0 and pattern[j] != pattern[i]) {
-            j = lps[j - 1];
+        while (j > 0 and s[j] != s[i]) {
+            j = pi[j - 1];
         }
 
-        if (pattern[j] == pattern[i]) {
-            lps[i] = j + 1;
+        if (s[j] == s[i]) {
+            pi[i] = j + 1;
         }
     }
 
-    return lps;
+    return pi;
 }
 
 /*
@@ -55,9 +55,9 @@ Find places where pattern p occurs in string t
 
 string s = p + ‘#’ + t
 
-Get prefix function of s (lps[i] <= p.size() because of the separator)
+Get prefix function of s (pi[i] <= p.size() because of the separator)
 
-If (lps[i] == p.size()) then we’ve found a match that ends at index i (starts at i - p.size() + 1)\
+If (pi[i] == p.size()) then we’ve found a match that ends at index i (starts at i - p.size() + 1)
 
 Because the suffix [i - p.size() + 1, i] == prefix (which is p)
 
@@ -65,25 +65,25 @@ Application 2 - counting number of occurrences of each prefix in a string:
 
 Counting the number of occurrences of each prefix s[0…i] in string s:
 
-lps[i] is the length of the longest proper prefix which is a suffix that ends at position i, shorter prefixes can end at the same position too.
+pi[i] is the length of the longest proper prefix which is a suffix that ends at position i, shorter prefixes can end at the same position too.
 
-So at position i, prefixes of the following sizes end: lps[i], lps[lps[i] - 1], lps[lps[lps[i] - 1] - 1] and so on until the index becomes zero
+So at position i, prefixes of the following sizes end: pi[i], pi[pi[i] - 1], pi[pi[pi[i] - 1] - 1] and so on until the index becomes zero
 
-vector<int> ans(n + 1);
+vector<int> freq(n + 1);
 
-// count the longest prefixes at each position (lps[i])
+// count the longest prefixes at each position (pi[i])
 for (int i = 0; i < n; i++) {
-    ans[lps[i]]++;
+    freq[pi[i]]++;
 }
 
 // accumulate smaller prefixes ending at the same position
 for (int i = n - 1; i > 0; i--) {
-    ans[lps[i - 1]] += ans[i];
+    freq[pi[i - 1]] += freq[i];
 }
 
 // count the suffixes themselves (each suffix occurs once at the start)
 for (int i = 0; i <= n; i++) {
-    ans[i]++;
+    freq[i]++;
 }
 
 Counting the number of occurrences of each prefix s[0…i] in string t:
@@ -97,13 +97,13 @@ string t = s + c, reverse t, now the task is to count the number of prefixes of 
 
 The total number of prefixes is |s| + 1.
 
-If we find the max value in the lps array of t (the longest prefix which appears as a substring in t) we know that all prefixes smaller that max(lps[i]) also appear so answer = |s| + 1 - max(lps[i])
+If we find the max value in the pi array of t (the longest prefix which appears as a substring in t) we know that all prefixes smaller that max(pi[i]) also appear so answer = |s| + 1 - max(pi[i])
 
-O(n2)
+O(n^2)
 
 Application 4 - compressing a string s (finding shortest t where t * n = s)
 
-k = n - lps[n - 1]
+k = n - pi[n - 1]
 
 If (n % k == 0) {
     ans = k;
